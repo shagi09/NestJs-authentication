50298d34929cd5016f45d63a11610fafcdfcf79b
shagi09 <shalomwubu024@gmail.com>
2025-07-24 21:28:27 +0300

    minor changes on users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
 
@@ -8,7 +6,5 @@ import { MongooseModule } from '@nestjs/mongoose';
   imports: [
     UsersModule,
     MongooseModule.forRoot('mongodb://localhost:27017/nestjs'),], // replace with your MongoDB URI],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
